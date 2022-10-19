874d539714d87712e0bdf763182cb8786ef729f8
KingNelx <tapiajonel606@gmail.com>
2022-10-19 16:58:50 +0800

    added top nav link

--- src/Student/StudentTopNav.jsx
+++ src/Student/StudentTopNav.jsx
@@ -1,27 +1,11 @@
 const StudentTopNav = () => {
-    return ( 
-        <div className="container">
-           
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
-              </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
-              </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="messages-tab" data-bs-toggle="tab" data-bs-target="#messages" type="button" role="tab" aria-controls="messages" aria-selected="false">Messages</button>
-              </li>
-            </ul>
-            
-           
-            <div class="tab-content">
-              <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab"> home </div>
-              <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab"> profile </div>
-              <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab"> messages </div>
-            </div>
+    return (
+        <div class="d-flex flex-row-reverse">
+            <div class="p-2"><button type="button" class="btn btn-outline-primary">Primary</button></div>
+            <div class="p-2"><button type="button" class="btn btn-outline-primary">Primary</button></div>
+            <div class="p-2"></div>
         </div>
-     );
+    );
 }
- 
+
 export default StudentTopNav;
