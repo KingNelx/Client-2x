220401e8d27df3bf133b2770295ea3af60fc466c
KingNelx <tapiajonel606@gmail.com>
2022-10-21 13:14:22 +0800

    added dashboards

--- src/Student/StudentDashB.jsx
+++ src/Student/StudentDashB.jsx
@@ -1,34 +1,47 @@
 const StudentDashB = () => {
-
     return (
-        <div className="container">
-            <table class="table ">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        <th scope="col"> Action </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>
-                            <button type="button" class="btn btn-outline-primary mx-auto">View</button>
-                            <button type="button" class="btn btn-outline-danger">Update</button>
-                            <button type="button" class="btn btn-outline-info">Drop</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="container mt-5 py-5">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <div class="card mx-auto" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="https://w7.pngwing.com/pngs/356/11/png-transparent-neet-pg-%C2%B7-2018-educational-entrance-examination-test-migration-agent-perth-criteria.png" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Check Requirements</h5>
+
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card mx-auto" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="https://advocateabroad.com/wp-content/uploads/2022/01/au-Final_icons_jan2022_service.png" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Submit Requirement </h5>
+
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card mx-auto" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Enroll</h5>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
- 
 export default StudentDashB;
