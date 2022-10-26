ab0260f94dfc92f63fc3a238cd92dc5a92bb971e
KingNelx <tapiajonel606@gmail.com>
2022-10-26 22:15:17 +0800

    added some test

--- src/Components/SubmitReq.jsx
+++ src/Components/SubmitReq.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom"
 const SubmitReq = () => {
     return (
         <div className="container">
-            <div class="mb-3">
+            STILL CODING
+            {/* <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="files" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
+            </div> */}
+            <Link to="/studentHOME">Go Back</Link>
         </div>
-        //  <Link to="/studentHOME">Go Back</Link>
+         
     );
 }
 
