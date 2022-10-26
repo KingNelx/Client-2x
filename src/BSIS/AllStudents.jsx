6767c24ca665ed4adee145932650c31901baee23
unknown <tapiajonel606@gmail.com>
2022-10-26 08:17:43 +0800

    added all students components

---
+++ src/BSIS/AllStudents.jsx
@@ -0,0 +1,33 @@
+const AllStudents = () => {
+    return (
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First name</th>
+                        <th scope="col">Last name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Grade Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Rielle</td>
+                        <td>Evangelista</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default AllStudents;
