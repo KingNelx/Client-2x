5a94a3e8dc43ba638538e343c7fd83f8ffc8a719
KingNelx <tapiajonel606@gmail.com>
2022-10-27 08:50:54 +0800

    edit button

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,7 +2,7 @@ const Buttons = () => {
     return (
         <div>
             <button type="button" class="btn btn-outline-primary mx-2">UPDATE</button>
-            <button type="button" class="btn btn-outline-danger mx-2">DELETE</button>
+            <button type="button" class="btn btn-outline-danger mx-2">DISABLE</button>
             <button type="button" class="btn btn-outline-info mx-2">VIEW</button>
         </div>
     );
